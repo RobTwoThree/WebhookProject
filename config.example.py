61d1a72adf8c65d6da87a090fa5c6083d002359e
RobTwoThree <rsayson31@gmail.com>
2018-10-04 18:59:53 -0700

    Create example config

---
+++ config.example.py
@@ -0,0 +1,9 @@
+# Fill in the following attributes to configure the webhook
+HOST = '127.0.0.1'
+PORT = 5005
+
+# Database attributes
+DB_HOST = 'localhost'
+DB_USER = 'user_name'
+DB_PASSWORD = 'password'
+DATABASE = 'database'
