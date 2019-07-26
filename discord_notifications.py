cff6f5ac1e8d5ceeb4655f3fbccad73eb4c606c5
RobTwoThree <rsayson31@gmail.com>
2019-07-25 19:58:44 -0700

    Initial working discord notification

--- discord_notifications.py
+++ discord_notifications.py
@@ -1,7 +1,7 @@
 import logging
-
+import time
 from discord_webhook import DiscordWebhook, DiscordEmbed
-from config import webhook_url
+from config import webhook_url, google_map_api_key
 
 webhook = DiscordWebhook(url=webhook_url, username="PoGoSD Rocket Bot")
 
@@ -14,18 +14,19 @@ def notify(data):
     pokestop_incident_start = data[0][5]
     pokestop_incident_expiration = data[0][6]
 
-    #start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(pokestop_incident_start))
-    #end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(pokestop_incident_expiration))
+    pokestop_name = "**" + str(pokestop_name) + " Pokestop**"
+    start_time = "**" + str(time.strftime('%I:%M %p',  time.localtime(pokestop_incident_start))) + "**"
+    end_time = "**" + str(time.strftime('%I:%M %p', time.localtime(pokestop_incident_expiration))) + "**"
+    map_url = "https://www.google.com/maps?q=" + str(pokestop_lat) + "," + str(pokestop_lon)
+    map_image = "https://maps.googleapis.com/maps/api/staticmap?center=" + str(pokestop_lat) + "," + str(pokestop_lon) + "&zoom=13&size=300x300&maptype=roadmap&markers=color:red%7Clabel:R%7C" + str(pokestop_lat) + "," + str(pokestop_lon) + "&key=" + str(google_map_api_key)
     
-    #print("START TIME: " + str(start_time))
-    #print("END TIME: " + str(end_time))
-    embed = DiscordEmbed(title=pokestop_name, description='Dark Pokestop has emerged!', color=242424)
+    embed = DiscordEmbed(title=pokestop_name, description='Dark Pokestop has emerged!', color=16714240)
 
     # set author
     #embed.set_author(name='Author Name', url='author url', icon_url='author icon url')
 
     # set image
-    #embed.set_image(url='your image url')
+    embed.set_image(url=map_image)
 
     # set thumbnail
     embed.set_thumbnail(url=pokestop_url)
@@ -37,11 +38,12 @@ def notify(data):
     embed.set_timestamp()
 
     # add fields to embed
-    embed.add_embed_field(name='Started:', value='Time 1')
-    embed.add_embed_field(name='Ends:', value='Time 2')
+    embed.add_embed_field(name='*Started:*', value=start_time)
+    embed.add_embed_field(name='*Ends:*', value=end_time)
+    embed.add_embed_field(name='Location:', value=map_url)
 
     webhook.add_embed(embed)
     
     webhook.execute()
-    print("Discord notification sent.")
+    print("Discord notification sent for: " + str(pokestop_name) + " START: " + str(start_time) + " END: " + str(end_time))
     return 'Discord notification sent.', 200
